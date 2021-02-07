fa5c638bdc982ff805f9753175f1f8b7253a387d
mikhail.krasilnikov <skater7878@gmail.com>
2021-02-07 17:55:54 +0300

    test

--- motion.py
+++ motion.py
@@ -1,29 +1,36 @@
 import time
+from datetime import timezone
 import uuid
 import json
 import datetime
 import RPi.GPIO as GPIO
+from gpiozero import MotionSensor
 import picamera
+import paho.mqtt.client as mqtt
+import os
+
 from minio import Minio
 from minio.error import S3Error
+from subprocess import call
 
-import paho.mqtt.client as mqtt
-
-GPIO.setmode(GPIO.BCM)
 
 PIR_PIN = 26
-VIDEO_TIME_SEC = 5
-BROKER_ADDRESS = "192.168.1.9"
+pir = MotionSensor(PIR_PIN)
+VIDEO_TIME_SEC = 15
+BROKER_ADDRESS = "185.251.89.148"
 
-MINIO_HOST = "192.168.1.9:443"
+MINIO_HOST = "185.251.89.148:443"
 BUCKET_NAME = 'raspberrycamera'
 
 FILE_DIR = 'snapshots/'
+MP4_VIDEO_EXT = '.mp4'
+H264_VIDEO_EXT = '.h264'
 camera = picamera.PiCamera()
 camera.resolution = 640,480
 GPIO.setup(PIR_PIN, GPIO.IN)
 
 mqttClient = mqtt.Client("P1")
+mqttClient.loop_start()
 mqttClient.connect(BROKER_ADDRESS)
 
 client = Minio(
@@ -55,11 +62,19 @@ def getDeviceId():
 
 
 def record(filename):
-    print("Recording : " + filename)
-    camera.start_recording(filename)
+    h264_file = filename + H264_VIDEO_EXT
+    print("Recording : " + h264_file)
+    camera.start_recording(h264_file)
     camera.wait_recording(VIDEO_TIME_SEC)
     camera.stop_recording()
     print("Recorded")
+    
+    mp4_file = filename + MP4_VIDEO_EXT
+    command = "MP4Box -add " + h264_file + " " + mp4_file
+    print("Converting from .h264 to mp4")
+    
+    call([command], shell=True)
+    print("Converted")
 
 
 def sendToMinio(filename):
@@ -69,40 +84,45 @@ def sendToMinio(filename):
             BUCKET_NAME, filename, FILE_DIR + filename
         )
         print("Video has been sent")
-    except Exception:
+    except Exception as e:
+        print(e)
         print("Couldn't send to Minio")
+        
+def removeFiles(filename):
+    if os.path.exists(FILE_DIR + filename + H264_VIDEO_EXT):
+        os.remove(FILE_DIR + filename + H264_VIDEO_EXT)
+    if os.path.exists(FILE_DIR + filename + MP4_VIDEO_EXT):
+        os.remove(FILE_DIR + filename + MP4_VIDEO_EXT)
 
 
-time.sleep(2)
+time.sleep(5)
 deviceUUID = getDeviceId()
-print('Ready. Device ID : {}'.format(deviceUUID))
 
 try:
     while True:
-        ts = time.time()
-        st = datetime.datetime.fromtimestamp(ts).strftime('%d.%m.%Y %H:%M:%S')
-        if GPIO.input(PIR_PIN):
-            time.sleep(0.5)
-            print("Motion Detected... Sending to MQTT")
-            alarmUUID = str(uuid.uuid4())
-            filename = '{}_alarm_{}.h264'.format(alarmUUID, st)
-            message = json.dumps({
-                                    'device_id': deviceUUID,
-                                    'id': alarmUUID,
-                                    'place': 'office_room',
-                                    'filename': filename,
-                                    'type': 'detected_motion',
-                                    'occurred_at': st
-                                    }, sort_keys=True)
-            mqttClient.publish("raspberry/main", message)
-            record(FILE_DIR + filename)
-            sendToMinio(filename)
-            
-            time.sleep(3)
-        time.sleep(0.1)
+        pir.wait_for_motion()
+        dt = datetime.datetime.utcnow()
+        st = dt.strftime('%d.%m.%Y %H:%M:%S')
+        print("Motion Detected at : " + st)
+        alarmUUID = str(uuid.uuid4())
+        filename = '{}_alarm'.format(alarmUUID)
+        message = json.dumps({
+                                'device_id': deviceUUID,
+                                'id': alarmUUID,
+                                'place': 'office_room',
+                                'filename': filename + MP4_VIDEO_EXT,
+                                'type': 'detected_motion',
+                                'occurred_at': st
+                                }, sort_keys=True)
+        mqttClient.publish("raspberry/main", message)
+        record(FILE_DIR + filename)
+        sendToMinio(filename + MP4_VIDEO_EXT)
+        removeFiles(filename)
+        time.sleep(8)
         
 except Exception as e:
     print(e)
     print("Quit")
+    mqttClient.loop_stop()
     GPIO.cleanup()
     
